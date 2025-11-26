948e60cba7a1f56e0ddc3f13941ff7d18f73648d
Matheus Leite <maatheus2710@gmail.com>
2025-11-26 02:06:46 -0300

    whipeoff

--- src/components/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import './Footer.css'
-
-export default function Footer() {
-  return (
-    <footer className='footer'>
-        <span className='footer-text'> Designed and coded by Matheus Leite - 2023 </span> 
-    </footer>
-  )
-}
